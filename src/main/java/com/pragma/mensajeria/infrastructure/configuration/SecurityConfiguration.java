be475b00138f5c1cf0074873eaf44bd8d87eeab4
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-06 13:03:35 -0500

    fix: remove Exception declaration from securityFilterChain method

--- src/main/java/com/pragma/mensajeria/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/com/pragma/mensajeria/infrastructure/configuration/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
