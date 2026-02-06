215c3a1b94e73a67da3c7be5e0d723b73ea810bb
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-06 14:18:24 -0500

    fix: update import for INotificationDtoMapper in NotificationHandlerTest

--- src/test/java/com/pragma/mensajeria/application/handler/NotificationHandlerTest.java
+++ src/test/java/com/pragma/mensajeria/application/handler/NotificationHandlerTest.java
@@ -2,7 +2,7 @@ package com.pragma.mensajeria.application.handler;
 
 import com.pragma.mensajeria.application.dto.NotificationResponseDto;
 import com.pragma.mensajeria.application.dto.OrderReadyNotificationRequestDto;
-import com.pragma.mensajeria.application.mapper.NotificationDtoMapper;
+import com.pragma.mensajeria.application.mapper.INotificationDtoMapper;
 import com.pragma.mensajeria.domain.api.INotificationServicePort;
 import com.pragma.mensajeria.domain.model.NotificationMessage;
 import com.pragma.mensajeria.domain.model.NotificationResult;
@@ -24,7 +24,7 @@ class NotificationHandlerTest {
     private INotificationServicePort notificationServicePort;
 
     @Mock
-    private NotificationDtoMapper notificationDtoMapper;
+    private INotificationDtoMapper notificationDtoMapper;
 
     @InjectMocks
     private NotificationHandler notificationHandler;
